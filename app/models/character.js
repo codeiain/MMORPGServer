8d3c5be1f7064e5377e93164c204c758bb34e7d4
Iain Brown <codeiain@outlook.com>
2017-05-08 11:36:51 +0100

    Updated Character

--- app/models/character.js
+++ app/models/character.js
@@ -6,42 +6,81 @@
 var mongoose = require('mongoose');
 
 var CharacterSchema = new mongoose.Schema({
-    playerId:{
-        type:String,
-        required:true
+    playerId: {
+        type: String,
+        required: true
     },
-    name:{
-        type:String,
-        unique:true,
-        required:true
+    name: {
+        type: String,
+        unique: true,
+        required: true
     },
-    Str:{
-        type:Number,
-        required:true
+    class: {
+        type: String,
+            required: true
     },
-    Dex:{
-        type:Number,
-        required:true
+    level: Number,
+    race: {
+        type: String,
+        required: true
     },
-    Con:{
-        type:Number,
-        required:true
+    gender: String,
+    str: {
+        type: Number,
+        required: true
     },
-    Int:{
-        type:Number,
-        required:true,
+    dex: {
+        type: Number,
+        required: true
     },
-    Wis:{
-        type:Number,
-        required:true
+    con: {
+        type: Number,
+        required: true
     },
-    Cha:{
-        type:Number,
-        required:true
+    int: {
+        type: Number,
+        required: true,
     },
-    Inventory:{
-        type : Array , 
-        "default" : []
+    wis: {
+        type: Number,
+        required: true
+    },
+    cha: {
+        type: Number,
+        required: true
+    },
+    inventory: {
+        type: Array,
+        "default": []
+    },
+    speed: Number,
+    size: String,
+    ac: Number,
+    touchAc: Number,
+    flatfootAc: Number,
+    hitPoints: Number,
+    skills: [{
+        name: String,
+        modifer: Number,
+        ranks: Number,
+        abilityModifer: Number,
+        miscModifer: Number,
+        untrained: Boolean,
+        armorPenalty: Boolean
+    }],
+    worn: {
+        head: String,
+        eyes: String,
+        neck: String,
+        shoulders: String,
+        ring1: String,
+        ring2: String,
+        hands: String,
+        arms: String,
+        body: String,
+        torso: String,
+        waist: String,
+        feet: String
     }
 }, {
     timestamps: true
